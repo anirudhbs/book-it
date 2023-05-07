8cd75da26f8d6f6dcea87cc9cce48be5d28f2679
Anirudh Bangalore Srinivas <anirudhbs96@gmail.com>
2023-05-07 22:42:12 +0530

    Scrape details for first upcoming match

--- src/index.js
+++ src/index.js
@@ -6,7 +6,30 @@ async function scrapePage() {
 
   await page.goto("https://www.hltv.org/matches?predefinedFilter=lan_only");
   const content = await page.content();
-  // console.log("pagep", content);
+  const matchSelector = ".upcomingMatch";
+  const upcomingMatches = await page.$$(matchSelector);
+  //  ideal shape: {id: "", url: "", team1: "", team2: "", time: "", event: ""}
+
+  const g = await upcomingMatches[0].evaluate((el) => {
+    const currentMatchData = {};
+    const url = el.querySelector("a.match").getAttribute("href");
+    const id = url.split("/")[2];
+    const team1 = el.querySelector(".team1 > .matchTeamName").textContent;
+    const team2 = el.querySelector(".team2 > .matchTeamName").textContent;
+    const time = el.querySelector(".matchTime").getAttribute("data-unix");
+
+    currentMatchData.id = id;
+    currentMatchData.url = url;
+    currentMatchData.team1 = team1;
+    currentMatchData.team2 = team2;
+    currentMatchData.time = time;
+
+    return currentMatchData;
+  }, await page.$(".matchTeamName"));
+  // const p = await matches[0].("textContent");
+  console.log("g", g);
+
+  console.log("total", upcomingMatches.length);
 }
 
 scrapePage();
