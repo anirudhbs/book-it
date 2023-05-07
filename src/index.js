9fb197e22e26a7e6296593b4fc375a1058d556b2
Anirudh Bangalore Srinivas <anirudhbs96@gmail.com>
2023-05-07 21:27:47 +0530

    Fetch page content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+const puppeteer = require("puppeteer");
+
+async function scrapePage() {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.hltv.org/matches?predefinedFilter=lan_only");
+  const content = await page.content();
+  // console.log("pagep", content);
+}
+
+scrapePage();
